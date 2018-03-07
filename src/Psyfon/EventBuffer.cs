a99077c808a48fc37e3bee32bec2d6291e1ca43e
Ali Kheyrollahi <aliostad@gmail.com>
2018-03-07 21:13:16 +0000

    wip x 2

--- src/Psyfon/EventBuffer.cs
+++ src/Psyfon/EventBuffer.cs
@@ -3,13 +3,14 @@ using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Threading;
 using Microsoft.Azure.EventHubs;
-
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace Psyfon
 {
     public class EventBuffer : IDisposable
     {
-        private ConcurrentQueue<EventData> _queue = new ConcurrentQueue<EventData>();
+        private ConcurrentQueue<Tuple<EventData, string>> _queue = new ConcurrentQueue<Tuple<EventData, string>>();
         private const int DefaultBatchSize = 128 * 1024; // 128KB
         private readonly IHasher _hasher;
         private readonly IEventHubClientWrapper _client;
@@ -18,6 +19,8 @@ namespace Psyfon
         private bool _isAccepting = true;
         private Thread _worker;
         private CancellationTokenSource _cancellationTokenSource = new CancellationTokenSource();
+        private ConcurrentDictionary<string, Lazy<PartitionCommitter>> _committers = new ConcurrentDictionary<string, Lazy<PartitionCommitter>>();
+
 
         public EventBuffer(string connectionString,
             int batchBufferSize = DefaultBatchSize,
@@ -45,42 +48,56 @@ namespace Psyfon
         public bool Add(EventData @event, string partitionKey = null)
         {
             if(_isAccepting)
-                _queue.Enqueue(@event);
+                _queue.Enqueue(new Tuple<EventData, string>(@event, partitionKey));
 
             return _isAccepting;
         }
-
-        public void Add(IEnumerable<EventData> events, string partitionKey = null)
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="events"></param>
+        /// <param name="partitionKey"></param>
+        /// <returns>Whether message was accepted. If one is not accepted it will be false.</returns>
+        public bool Add(IEnumerable<EventData> events, string partitionKey = null)
         {
-            foreach (var item in events)
-            {
-                Add(item, partitionKey);
-            }
+            return events.Select(x => Add(x, partitionKey)).Aggregate((a,b) => a && b);
         }
 
         private void Work()
         {
             while (!_cancellationTokenSource.Token.IsCancellationRequested)
             {
-                // _queue.TryDequeue()
+                Tuple<EventData, string> data;
+                if(_queue.TryDequeue(out data))
+                {
+                    var pk = _hasher.Hash(data.Item2 ?? Guid.NewGuid().ToString(), _partitionCount);
+                    var committer = _committers.GetOrAdd(pk,
+                        new Lazy<PartitionCommitter>(() => new PartitionCommitter(_client, _batchBufferSize, pk)));
+                    committer.Value.Add(data.Item1);
+                }
+                else
+                {
+                    Task.Delay(100, _cancellationTokenSource.Token).GetAwaiter().GetResult();
+                }
             }
         }
 
         public void Dispose()
         {
             _isAccepting = false;
-            Flush();
+            _cancellationTokenSource.Cancel();
+            foreach (var kv in _committers)
+            {
+                kv.Value.Value.Dispose();
+            }
+
             _client.Dispose();
         }
 
         public void Start()
         {
-
-        }
-
-        private void Flush()
-        {
-
+            _worker = new Thread(Work);
+            _worker.Start();
         }
     }
 }
